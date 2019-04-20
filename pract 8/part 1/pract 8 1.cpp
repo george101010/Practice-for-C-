9dae74fd9278f077eeae39629adbd9ae0da71838
george101010 <47598032+george101010@users.noreply.github.com>
2019-04-20 21:12:53 +0300

    .

---
+++ pract 8/part 1/pract 8 1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main() {
+
+  string snt;
+  ifstream fl("text1.txt");//открытие файла
+   while (getline(fl, snt)) //считывание строк из файла
+        {
+            int n=snt.length();
+            for (int i=0;i<n;i++){//печать каждого слова на одной строке
+                if (snt[i]==' '){
+                    cout<<"\n";
+                }
+                else {cout<<snt[i];}
+            }
+            cout<<"\n";
+        }
+
+  fl.close();//закрытие файла
+  return 0;
+}
---
+++ pract 8/part 2/pract 8 2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main() {
+  ofstream fl;
+  string snt="";
+  fl.open("text.txt");//открытие файла
+  while (snt!="^^"){
+    getline(cin,snt);//запись текста в  файл
+    fl<<snt<<endl;
+    }
+  fl.close();
+  return 0;
+}
